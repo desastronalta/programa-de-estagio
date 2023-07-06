280a2b6a8266b205537112f37b5ad2f43822afd3
Rafael Dantas <rafadantasdasilva54@gmail.com>
2023-07-06 20:28:06 -0300

    primeiro commit duplicados

---
+++ programa estagio/src/metodos/Duplicados.java
@@ -0,0 +1,16 @@
+package metodos;
+
+import java.util.ArrayList;
+
+public class Duplicados {
+	private StringBuffer semDuplicadas;
+	private String frase;
+	private ArrayList<String> palavras;
+	
+	public Duplicados() {
+		palavras = new ArrayList<String>();
+		semDuplicadas = new StringBuffer();
+	}
+	
+	
+}
