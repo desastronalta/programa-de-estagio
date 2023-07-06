493dd7d150d707be72b59cb82a237dc5a2e32f98
Rafael Dantas <rafadantasdasilva54@gmail.com>
2023-07-06 12:34:52 -0300

    first commit of the project

---
+++ programa estagio/src/metodos/Reverse.java
@@ -0,0 +1,23 @@
+package metodos;
+
+public class Reverse {
+	//var's
+	private String frase;
+	private StringBuffer inverse;
+	
+	//getters and setters
+	public String getFrase() {
+		return frase;
+	}
+	public StringBuffer getInverse() {
+		return inverse;
+	}
+	public void setFrase(String frase) {
+		this.frase = frase;
+	}
+	public void setInverse(StringBuffer inverse) {
+		this.inverse = inverse;
+	}
+	
+	
+}
