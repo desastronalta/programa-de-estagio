749fbc62cb700b996c908587fb6a3e5dee534b24
Rafael Dantas <rafadantasdasilva54@gmail.com>
2023-07-06 13:23:16 -0300

    planejamento do metodo de inverter Strings

--- programa estagio/src/metodos/Reverse.java
+++ programa estagio/src/metodos/Reverse.java
@@ -1,10 +1,16 @@
 package metodos;
+import java.util.ArrayList;
 
 public class Reverse {
 	//var's
 	private String frase;
 	private StringBuffer inverse;
+	private ArrayList<String> palavras;
 	
+	public Reverse(){
+		this.inverse = new StringBuffer();
+		this.palavras = new ArrayList<String>();
+	}
 	//getters and setters
 	public String getFrase() {
 		return frase;
@@ -18,6 +24,29 @@ public class Reverse {
 	public void setInverse(StringBuffer inverse) {
 		this.inverse = inverse;
 	}
+	//metodos de inversão
+	private void findWords(){
+		String palavra = null;
+		int lim = 0;
+		for(int i = 0; i < this.frase.length(); i++) {
+			char letra = this.frase.charAt(i);
+			if(letra == ' ') {
+				palavras.add(getWord(i,lim));
+				lim = i;
+			}
+		}
+	}
 	
+	public void inverteString(){
+		
+	}
 	
+	private String getWord(int i, int lim) {
+		StringBuffer word = new StringBuffer();
+		for(int j = lim ;j < i; j++) {
+			
+		}
+		String palavra = word.toString() ;
+		return palavra;
+	}
 }
