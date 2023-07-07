38ea36ecb60eb53c8683dfa238ece972d76352e0
Rafael Dantas <rafadantasdasilva54@gmail.com>
2023-07-06 21:09:25 -0300

    primeira versão do segundo desafio

--- programa estagio/src/metodos/Duplicados.java
+++ programa estagio/src/metodos/Duplicados.java
@@ -1,16 +1,49 @@
 package metodos;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class Duplicados {
 	private StringBuffer semDuplicadas;
 	private String frase;
-	private ArrayList<String> palavras;
+	//private ArrayList<String> palavras;
+	
+	public String getFrase() {
+		return frase;
+	}
+	public void setFrase(String frase) {
+		this.frase = frase;
+	}
 	
 	public Duplicados() {
-		palavras = new ArrayList<String>();
+		//palavras = new ArrayList<String>();
 		semDuplicadas = new StringBuffer();
 	}
 	
+	public void addLetra() {
+		String palavra = this.frase;
+		for(int i = 0; i < palavra.length(); i++ ) {
+			char letra = palavra.charAt(i);
+			if(i == 0) {
+				this.semDuplicadas.append(letra);
+			}else if(verificaDupli(letra)) {
+				this.semDuplicadas.append(letra);
+			}
+		}
+	System.out.println(this.semDuplicadas);
+	}
+	private boolean verificaDupli(char letra) {
+		String palavra = this.semDuplicadas.toString();
+		boolean teste = true;
+		int count = 0;
+		for(int i = 0; i< palavra.length(); i++) {
+			if(letra == palavra.charAt(i)) {
+				count ++;
+			}
+		}
+		if(count > 0 ) {
+			teste = false;
+		}
+		return teste;
+	}
 	
 }
