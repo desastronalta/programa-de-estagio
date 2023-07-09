da11e73597dd5fed7e59e7c8a9011d86f87b828c
Rafael Dantas <rafadantasdasilva54@gmail.com>
2023-07-09 15:33:08 -0300

    tela para execução do programa.

--- programa estagio/src/executavel/Executavel.java
+++ programa estagio/src/executavel/Executavel.java
@@ -8,13 +8,24 @@ import java.awt.BorderLayout;
 import javax.swing.JTextField;
 import java.awt.Font;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
+
+import metodos.*;
+
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JRadioButton;
+import javax.swing.JTextPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
 
 public class Executavel {
 
 	private JFrame frame;
 	private JTextField textField;
+	private final JLabel lblNewLabel_2 = new JLabel("Descrição:");
 
 	/**
 	 * Launch the application.
@@ -66,27 +77,105 @@ public class Executavel {
 		panel.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Insira o texto desejado, abaixo:");
-		lblNewLabel_1.setBounds(93, 74, 172, 14);
+		lblNewLabel_1.setBounds(93, 74, 233, 14);
 		panel.add(lblNewLabel_1);
 		
 		JButton reverse = new JButton("Desafio 1");
-		reverse.setBounds(85, 169, 89, 23);
+		reverse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Reverse rev = new Reverse();
+				if(verificaCampoText()) {
+					rev.setFrase("Hello, world!, OpenAIis amazing.");
+					JOptionPane.showMessageDialog(null, rev.inverteString());
+				}else {
+					rev.setFrase(textField.getText());
+					JOptionPane.showMessageDialog(null, rev.inverteString());
+				}
+				
+			}
+		});
+		reverse.setBounds(10, 145, 104, 33);
 		panel.add(reverse);
 		
-		JButton duplicados = new JButton("Desafio 3");
-		duplicados.setBounds(199, 169, 89, 23);
+		JButton duplicados = new JButton("Desafio 2");
+		duplicados.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Duplicados dup = new Duplicados();
+				if(verificaCampoText()) {
+					dup.setFrase("Hello World!");
+					JOptionPane.showMessageDialog(null, dup.mostrarSemDupli());
+				}else {
+					dup.setFrase(textField.getText());
+					JOptionPane.showMessageDialog(null, dup.mostrarSemDupli());
+				}
+				
+			}
+		});
+		duplicados.setBounds(116, 145, 104, 33);
 		panel.add(duplicados);
 		
-		JButton palindromos = new JButton("Desafio 2");
-		palindromos.setBounds(85, 243, 89, 23);
+		JButton palindromos = new JButton("Desafio 3");
+		palindromos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Palindromos pali = new Palindromos();
+				if(verificaCampoText()) {
+					pali.setPalavra("babad");
+					JOptionPane.showMessageDialog(null,pali.getMaiorPalin());
+				}else {
+					pali.setPalavra(textField.getText());
+					JOptionPane.showMessageDialog(null,pali.getMaiorPalin());
+				}
+			}
+		});
+		palindromos.setBounds(222, 145, 104, 33);
 		panel.add(palindromos);
 		
 		JButton maiusculas = new JButton("Desafio 4");
-		maiusculas.setBounds(199, 243, 89, 23);
+		maiusculas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(verificaCampoText()) {
+					Maiusculas m = new Maiusculas("hello. how are you? i'm fine, thank you.");
+					JOptionPane.showMessageDialog(null, m.mostraFraseMod());
+				}else {
+					Maiusculas m = new Maiusculas(textField.getText());
+					JOptionPane.showMessageDialog(null, m.mostraFraseMod());
+				}
+			}
+		});
+		maiusculas.setBounds(328, 145, 104, 33);
 		panel.add(maiusculas);
 		
 		JButton anagrama = new JButton("Desafio 5");
-		anagrama.setBounds(315, 205, 89, 23);
+		anagrama.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(verificaCampoText()) {
+					Anagrama anag = new Anagrama("racecar");
+					JOptionPane.showMessageDialog(null, anag.comparaAnagram());
+				}else {
+					Anagrama anag = new Anagrama(textField.getText());
+					JOptionPane.showMessageDialog(null, anag.comparaAnagram());
+				}
+			}
+		});
+		anagrama.setBounds(435, 145, 104, 32);
 		panel.add(anagrama);
+		lblNewLabel_2.setBounds(20, 190, 98, 31);
+		panel.add(lblNewLabel_2);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(93, 221, 362, 157);
+		panel.add(scrollPane);
+		
+		JTextPane txtpnOsDesafiosConsistem = new JTextPane();
+		scrollPane.setViewportView(txtpnOsDesafiosConsistem);
+		txtpnOsDesafiosConsistem.setEditable(false);
+		txtpnOsDesafiosConsistem.setText("\tOs desafios consistem em manipulação  de String onde cada desafio a manipula de forma diferente, o desafio 1 consistem em inverter a ordem das palavras,(ex: ola mundo = mundo ola). \r\nO desafio 2 consiste em retirar as letras duplicadas das frases(ex: hello world = helo wrd). \r\nO desafio 3 consiste em encontrar o maior palindromo dentro da palavra (ex: rafa = afa).\r\nO desafio 4 consiste  em colocar em colocar as letras pós pontuação em maiuscula quando necessário (ex: ola mundo. sou feliz = Ola mundo. Sou feliz). \r\nO desafio 5 consiste em analisar se o anagrama de uma palavra é palindromo(ex: racecar = racecar logo true).\r\n\r\nObs: caso queira utilizar um exemplo invés de digitar o texto apenas deixe o campo em branco.\r\nsegue abaixo as legendas utilizadas  como exemplo caso o campo de texto fique em branco:\r\n\r\n-Desafio 1 : \"Hello, Wolrd! OpenAI is amazing\";\r\n\r\n-Desafio 2: \"Hello, World\";\r\n\r\n-Desafio 3: \"babad\";\r\n\r\n-Desafio 4: \"hello. how are you? i'm fine, thank you\";\r\n\r\n-Desafio 5: \"racecar\".");
+	}
+	private boolean verificaCampoText() {
+		boolean flag = false;
+		if(textField.getText().equals("")) {
+			flag = true;
+		}
+		return flag;
 	}
 }
